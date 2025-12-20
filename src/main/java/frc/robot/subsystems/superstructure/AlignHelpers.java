c4cd5a157e2aa94a891b7bcfb2d82196c9a06deb
amsam0 <44983869+amsam0@users.noreply.github.com>
2025-12-20 12:01:47 -0800

    Use Util.error if an AprilTag can't be found instead of crashing

--- src/main/java/frc/robot/subsystems/superstructure/AlignHelpers.java
+++ src/main/java/frc/robot/subsystems/superstructure/AlignHelpers.java
@@ -3,14 +3,19 @@ package frc.robot.subsystems.superstructure;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform2d;
+import frc.lib.Util;
 import frc.robot.subsystems.apriltagvision.AprilTagVisionConstants;
 
 import static frc.robot.subsystems.drive.DriveConstants.driveConfig;
 
 public class AlignHelpers {
-    @SuppressWarnings("OptionalGetWithoutIsPresent") // better for our code to crash than to fail silently
     public static Pose2d getAprilTagPose(int id) {
-        return AprilTagVisionConstants.aprilTagLayout.getTagPose(id).get().toPose2d();
+        var tagPoseOpt = AprilTagVisionConstants.aprilTagLayout.getTagPose(id);
+        if (tagPoseOpt.isEmpty()) {
+            Util.error("Could not find pose for AprilTag with ID " + id);
+            return new Pose2d();
+        }
+        return tagPoseOpt.get().toPose2d();
     }
 
     public static final Transform2d bumperOffset = new Transform2d(driveConfig.bumperLengthMeters() / 2.0, 0, new Rotation2d());
