06ce3dcd264aa80f8a936540fa2f7fc90d0819ab
amsam0 <44983869+amsam0@users.noreply.github.com>
2026-01-15 17:41:02 -0800

    Fix slip current characterization tracking current usage after movement instead of before movement

--- src/main/java/frc/robot/subsystems/drive/goals/SlipCurrentCharacterizationGoal.java
+++ src/main/java/frc/robot/subsystems/drive/goals/SlipCurrentCharacterizationGoal.java
@@ -10,6 +10,7 @@ public class SlipCurrentCharacterizationGoal extends DriveGoal {
 
     private final Timer timer = new Timer();
 
+    private double[] lastCurrents = new double[4];
     private final double[] currentNeededForMovement = new double[4];
     private boolean done = false;
 
@@ -26,28 +27,32 @@ public class SlipCurrentCharacterizationGoal extends DriveGoal {
         double[] velocities = drive.getSlipCurrentCharacterizationVelocities();
         double[] currents = drive.getSlipCurrentCharacterizationCurrents();
 
-        double total = 0.0;
-        done = true;
-        for (int i = 0; i < 4; i++) {
-            if (velocities[i] > 0.01 && currentNeededForMovement[i] == 0) {
-                currentNeededForMovement[i] = currents[i];
-            }
-
-            if (done && currentNeededForMovement[i] != 0) {
-                total += currentNeededForMovement[i];
-            } else {
-                done = false;
-            }
-        }
-
-        if (done) {
-            double avg = total / 4.0;
-            System.out.println("Average current for each module to move: " + avg);
+        if (lastCurrents[0] != 0) {
+            double total = 0.0;
+            done = true;
             for (int i = 0; i < 4; i++) {
-                System.out.println("\tCurrent for module " + i + " to move: " + currentNeededForMovement[i]);
+                if (velocities[i] > 0.01 && currentNeededForMovement[i] == 0) {
+                    currentNeededForMovement[i] = lastCurrents[i];
+                }
+
+                if (done && currentNeededForMovement[i] != 0) {
+                    total += currentNeededForMovement[i];
+                } else {
+                    done = false;
+                }
             }
-            return DriveRequest.stop();
+
+            if (done) {
+                double avg = total / 4.0;
+                System.out.println("Average current for each module to move: " + avg);
+                for (int i = 0; i < 4; i++) {
+                    System.out.println("\tCurrent for module " + i + " to move: " + currentNeededForMovement[i]);
+                }
+                return DriveRequest.stop();
+            }
         }
+
+        lastCurrents = currents;
 
         // Increase voltage at 0.5 V/s
         return DriveRequest.characterization(timer.get() * 0.5);
