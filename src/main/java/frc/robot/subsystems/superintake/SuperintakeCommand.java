102ec791397a20cb3f17346a54c59720823e8001
amsam0 <44983869+amsam0@users.noreply.github.com>
2026-01-17 15:56:42 -0800

    Add intake rollers to superintake

--- src/main/java/frc/robot/subsystems/superintake/SuperintakeCommand.java
+++ src/main/java/frc/robot/subsystems/superintake/SuperintakeCommand.java
@@ -7,16 +7,19 @@ import frc.robot.RobotState;
 import frc.robot.subsystems.apriltagvision.AprilTagVision;
 import frc.robot.subsystems.drive.Drive;
 import frc.robot.subsystems.gamepiecevision.GamePieceVision;
+import frc.robot.subsystems.superintake.intakerollers.IntakeRollers;
 
 public abstract class SuperintakeCommand {
     protected final RobotState robotState = RobotState.get();
     protected final OperatorDashboard operatorDashboard = OperatorDashboard.get();
     protected final Controller controller = Controller.get();
 
-    protected final Superintake superintake = Superintake.get();
     protected final AprilTagVision aprilTagVision = AprilTagVision.get();
     protected final Drive drive = Drive.get();
     protected final GamePieceVision gamePieceVision = GamePieceVision.get();
+
+    protected final Superintake superintake = Superintake.get();
+    protected final IntakeRollers intakeRollers = IntakeRollers.get();
 
     public abstract Command create();
 
