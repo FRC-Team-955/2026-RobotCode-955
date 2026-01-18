e02df2da6f01da5efa9df12bf00112c33687616d
amsam0 <44983869+amsam0@users.noreply.github.com>
2026-01-18 08:30:42 -0800

    Limit acceleration while in heading override

--- src/main/java/frc/robot/subsystems/drive/goals/DriveJoystickWithHeadingOverrideGoal.java
+++ src/main/java/frc/robot/subsystems/drive/goals/DriveJoystickWithHeadingOverrideGoal.java
@@ -1,9 +1,10 @@
 package frc.robot.subsystems.drive.goals;
 
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.wpilibj.Timer;
-import frc.lib.PIDF;
+import frc.lib.SlewRateLimiter2d;
 import frc.lib.network.LoggedTunableNumber;
 import frc.robot.Controller;
 import frc.robot.RobotState;
@@ -13,31 +14,46 @@ import frc.robot.subsystems.drive.DriveRequest;
 import lombok.RequiredArgsConstructor;
 import org.littletonrobotics.junction.Logger;
 
-import static frc.robot.subsystems.drive.DriveConstants.driveConfig;
 import static frc.robot.subsystems.drive.DriveTuning.headingOverrideGainsTunable;
 
 @RequiredArgsConstructor
 public class DriveJoystickWithHeadingOverrideGoal extends DriveGoal {
+    private static final LoggedTunableNumber maxAccelerationMetersPerSecSquared = new LoggedTunableNumber("Drive/DriveJoystickWithHeadingOverride/MaxAccelerationMetersPerSecSquared", 5);
+
     private static final RobotState robotState = RobotState.get();
     private static final Controller controller = Controller.get();
     private static final ShootingKinematics shootingKinematics = ShootingKinematics.get();
 
+    private final SlewRateLimiter2d linearAccelerationLimiter = new SlewRateLimiter2d(maxAccelerationMetersPerSecSquared.get(), robotState.getMeasuredChassisSpeeds());
     private final PIDController headingOverride = headingOverrideGainsTunable.getOrOriginal().toPIDWrapRadians();
+
+    private Rotation2d lastLinearDirection = new Rotation2d();
 
     @Override
     public DriveRequest getRequest() {
         headingOverrideGainsTunable.ifChanged(gains -> gains.applyPID(headingOverride));
+        if (maxAccelerationMetersPerSecSquared.hasChanged()) {
+            linearAccelerationLimiter.setLimit(maxAccelerationMetersPerSecSquared.get());
+        }
 
         //////////////////////////////////////////////////////////////////////
 
-        ChassisSpeeds joystickSetpoint = controller.getDriveSetpointRobotRelative(robotState.getRotation());
+        Rotation2d linearDirection = controller.getDriveLinearDirection();
+        double linearMagnitude = controller.getDriveLinearMagnitude();
+        if (linearMagnitude == 0.0) {
+            linearDirection = lastLinearDirection;
+        } else {
+            lastLinearDirection = linearDirection;
+        }
+        Translation2d linearVelocity = new Translation2d(linearMagnitude, linearDirection);
+        linearVelocity = linearAccelerationLimiter.calculate(linearVelocity);
+        Logger.recordOutput("Drive/DriveJoystickWithHeadingOverride/LinearMagnitudeLimited", linearVelocity.getNorm());
 
-        joystickSetpoint = new ChassisSpeeds(
-                joystickSetpoint.vxMetersPerSecond,
-                joystickSetpoint.vyMetersPerSecond,
-                headingOverride.calculate(robotState.getRotation().getRadians(), shootingKinematics.getShootingParameters().headingRad())
-        );
-
-        return DriveRequest.chassisSpeeds(joystickSetpoint);
+        return DriveRequest.chassisSpeeds(ChassisSpeeds.fromFieldRelativeSpeeds(
+                linearVelocity.getX(),
+                linearVelocity.getY(),
+                headingOverride.calculate(robotState.getRotation().getRadians(), shootingKinematics.getShootingParameters().headingRad()),
+                robotState.getRotation()
+        ));
     }
 }
