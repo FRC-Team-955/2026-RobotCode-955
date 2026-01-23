6d0bbb07383fb19cd3dcfcf839e3efeef24ca759
PioneerSwine996 <pioneerswine9@gmail.com>
2026-01-23 11:27:23 -0800

    Remove Voltage/Current Limiter

--- src/main/java/frc/lib/motor/MotorIOArmSim.java
+++ src/main/java/frc/lib/motor/MotorIOArmSim.java
@@ -44,30 +44,6 @@ public class MotorIOArmSim extends MotorIO {
     public void updateInputs(MotorIOInputs inputs) {
         if (closedLoop) {
             appliedVolts = pid.calculate(armSim.getAngleRads()) + ff.calculate(armSim.getAngleRads(), 0);
-            appliedVolts = MathUtil.clamp(appliedVolts, -voltageLimit, voltageLimit);
-
-            // https://github.com/Shenzhen-Robotics-Alliance/maple-sim/blob/27ef554d86a62d8dba1a361cc5eca8919d4be1f9/project/src/main/java/org/ironmaple/simulation/motorsims/SimulatedMotorController.java#L63-L84
-            final double kCurrentThreshold = 1.2;
-
-            final double motorCurrentVelocityRadPerSec = armSim.getVelocityRadPerSec();
-            final double requestedOutputVoltageVolts = appliedVolts;
-            final double currentAtRequestedVoltageAmps = motor.getCurrent(motorCurrentVelocityRadPerSec, requestedOutputVoltageVolts);
-
-            // Resource for current limiting:
-            // https://file.tavsys.net/control/controls-engineering-in-frc.pdf (sec 12.1.3)
-            double limitedVoltage = requestedOutputVoltageVolts;
-            final boolean currentTooHigh =
-                    Math.abs(currentAtRequestedVoltageAmps) > (kCurrentThreshold * currentLimitAmps);
-            if (currentTooHigh) {
-                final double limitedCurrent = Math.copySign(currentLimitAmps, currentAtRequestedVoltageAmps);
-                limitedVoltage = motor.getVoltage(motor.getTorque(limitedCurrent), motorCurrentVelocityRadPerSec);
-            }
-
-            // ensure the current limit doesn't cause an increase to output voltage
-            if (Math.abs(limitedVoltage) > Math.abs(requestedOutputVoltageVolts))
-                limitedVoltage = requestedOutputVoltageVolts;
-
-            appliedVolts = limitedVoltage;
         }
 
         armSim.setInputVoltage(appliedVolts);
