39a9bb4f7f216186c7d0eabf6d4627f9f7e37c05
PioneerSwine996 <pioneerswine9@gmail.com>
2026-02-15 21:01:06 -0800

    Revert

--- src/main/java/frc/robot/subsystems/gamepiecevision/GamePieceVision.java
+++ src/main/java/frc/robot/subsystems/gamepiecevision/GamePieceVision.java
@@ -73,85 +73,74 @@ public class GamePieceVision implements Periodic {
                 var robotPose = new Pose3d(robotPose2d.get());
                 Translation2d targetYawPitch = new Translation2d(observation.yawRad(), observation.pitchRad());
                 targetPoints.add(targetYawPitch);
-
-                // Calculate distance using: theta = gamepieceWidthInPixels / pixelToRad
-                //                           distance = GamepieceDiameter / tan(theta)
-                double theta = observation.gamePieceWidthInPixels() / pixelsToRad;
-                double distance = fuelDiameterMeters / Math.tan(theta);
-
+                // Account for roll of camera
+                // TODO: fix roll compensation - this doesn't fully work
+                double theta = (observation.gamePieceWidthInPixels()) / pixelsToRad;
                 Logger.recordOutput("GamePieceVision/theta", theta);
-                Logger.recordOutput("GamePieceVision/distance", distance);
 
-                // CamToGamepiece = Vector(Distance, Heading + Cam_yaw + AngleToGamepiece)
-                // We need the camera's horizontal facing direction in field coordinates
 
-                // Get the camera's rotation in field coordinates
-                Pose3d camPoseField = robotPose.transformBy(metadata.robotToCamera);
-
-                // To find the camera's horizontal facing direction, we need to find where
-                // the camera's forward axis (+X in the transformed frame) points in the XY plane
-                // Create a unit vector pointing forward in camera space and transform it
-                Translation3d forwardInCam = new Translation3d(1, 0, 0);
-                // Rotate this vector by the camera's field rotation to get the direction it's pointing
-                Rotation3d camRotationField = camPoseField.getRotation();
-                Translation3d forwardInField = forwardInCam.rotateBy(camRotationField);
-
-                // Project onto XY plane and get the angle
-                double camHeadingField = Math.atan2(forwardInField.getY(), forwardInField.getX());
-
-                // PhotonVision yaw: positive = target is to the left of center
-                // Our convention: positive angle = counter-clockwise
-                // So we need to negate the yaw to convert from PhotonVision convention
-                double angleToGamepiece = -observation.yawRad();
-                double totalAngle = camHeadingField + angleToGamepiece;
-
-                Logger.recordOutput("GamePieceVision/camHeadingField", Math.toDegrees(camHeadingField));
-                Logger.recordOutput("GamePieceVision/angleToGamepiece", Math.toDegrees(angleToGamepiece));
-                Logger.recordOutput("GamePieceVision/totalAngle", Math.toDegrees(totalAngle));
-
+                double distance = (fuelDiameterMeters) / Math.tan(theta);
+                Logger.recordOutput("GamePieceVision/distanceX", distance);
+                double totalAngle = metadata.robotToCamera.getRotation().getZ() + robotPose.toPose2d().getRotation().getRadians() + observation.yawRad();
                 Translation2d camToGamepiece = new Translation2d(distance, new Rotation2d(totalAngle));
+                Pose3d camPose = robotPose.transformBy(metadata.robotToCamera);
 
-                // GamepiecePose = RobotPose + RobotToCam + CamToGamepiece
-                // Get camera position in field coordinates
-                Translation2d camPositionField = camPoseField.toPose2d().getTranslation();
+                //    +;;).plus(camToGamepiece))
+                Pose2d gamepiecePose = new Pose2d(camPose.toPose2d().getTranslation().plus(camToGamepiece), new Rotation2d());
 
-                // Add camToGamepiece vector to get final gamepiece position
-                Translation2d gamepiecePosition = camPositionField.plus(camToGamepiece);
-                Pose3d gamepiecePose = new Pose3d(gamepiecePosition.getX(), gamepiecePosition.getY(), fuelDiameterMeters / 2.0, new Rotation3d());
+                targetYawPitch = targetYawPitch.rotateBy(Rotation2d.fromRadians(-metadata.robotToCamera.getRotation().getX()));
+                double targetYaw = targetYawPitch.getX();
+                double targetPitch = targetYawPitch.getY();
 
-                Logger.recordOutput("GamePieceVision/camToGamepieceX", camToGamepiece.getX());
-                Logger.recordOutput("GamePieceVision/camToGamepieceY", camToGamepiece.getY());
+                // First, calculate position of target in camera space
+                double camToTargetZ = -metadata.robotToCamera.getZ() + fuelDiameterMeters / 2.0;
+                // Account for pitch of camera
+                double camToTargetX = camToTargetZ / Math.tan(targetPitch - metadata.robotToCamera.getRotation().getY());
+                double camToTargetY = camToTargetX * Math.tan(-targetYaw);
+
+                Translation2d camToTargetXY = new Translation2d(camToTargetX, camToTargetY)
+                        // Account for yaw of camera
+                        // Note that we do this BEFORE translating to robot coordinates
+                        .rotateBy(Rotation2d.fromRadians(metadata.robotToCamera.getRotation().getZ()));
+                // Next, translate x and y to robot coordinates
+                Translation2d robotToTargetXY = metadata.robotToCamera.getTranslation().toTranslation2d().plus(camToTargetXY);
+                double robotToTargetZ = metadata.robotToCamera.getZ() + camToTargetZ;
+
+                Translation3d robotToTarget = new Translation3d(robotToTargetXY.getX(), robotToTargetXY.getY(), robotToTargetZ);
+                Logger.recordOutput("GamePieceVision/tagX", camToTargetXY.getX());
 
                 // TODO: kalman filter or PoseEstimator for stability?
-                newlySeenCoral.put(gamepiecePose, observation.timestampSeconds());
+                newlySeenCoral.put(
+                        new Pose3d(gamepiecePose),
+                        observation.timestampSeconds());
+            }
+// robotPose.transformBy(new Transform3d(robotToTarget, new Rotation3d()))
+
+            // Handle newly seen coral
+            for (var pose : newlySeenCoral.keySet()) {
+                // Remove old coral within distance to be counted as the same piece of coral
+                coralPoseToLastSeen.keySet()
+                        .removeIf(otherPose -> pose.getTranslation().getDistance(otherPose.getTranslation()) < minDistanceForSameCoralMeters);
+            }
+            coralPoseToLastSeen.putAll(newlySeenCoral);
+
+            // Remove expired coral
+            coralPoseToLastSeen.values().removeIf(lastSeen -> Timer.getTimestamp() - lastSeen > staleExpireTimeSeconds);
+
+            // Generate fresh/stale arrays
+            freshCoral = new LinkedList<>();
+            staleCoral = new LinkedList<>();
+            for (var entry : coralPoseToLastSeen.entrySet()) {
+                Pose3d pose = entry.getKey();
+                double lastSeen = entry.getValue();
+
+                if (Timer.getTimestamp() - lastSeen < freshExpireTimeSeconds) {
+                    freshCoral.add(pose);
+                } else {
+                    staleCoral.add(pose);
+                }
             }
         }
-
-        // Handle newly seen coral
-        for (var pose : newlySeenCoral.keySet()) {
-            // Remove old coral within distance to be counted as the same piece of coral
-            coralPoseToLastSeen.keySet()
-                    .removeIf(otherPose -> pose.getTranslation().getDistance(otherPose.getTranslation()) < minDistanceForSameCoralMeters);
-        }
-        coralPoseToLastSeen.putAll(newlySeenCoral);
-
-        // Remove expired coral
-        coralPoseToLastSeen.values().removeIf(lastSeen -> Timer.getTimestamp() - lastSeen > staleExpireTimeSeconds);
-
-        // Generate fresh/stale arrays
-        freshCoral = new LinkedList<>();
-        staleCoral = new LinkedList<>();
-        for (var entry : coralPoseToLastSeen.entrySet()) {
-            Pose3d pose = entry.getKey();
-            double lastSeen = entry.getValue();
-
-            if (Timer.getTimestamp() - lastSeen < freshExpireTimeSeconds) {
-                freshCoral.add(pose);
-            } else {
-                staleCoral.add(pose);
-            }
-        }
-
         // Log results
         Logger.recordOutput("GamePieceVision/TargetPoints", targetPoints.toArray(Translation2d[]::new));
         Logger.recordOutput("GamePieceVision/FreshCoral", freshCoral.toArray(Pose3d[]::new));
