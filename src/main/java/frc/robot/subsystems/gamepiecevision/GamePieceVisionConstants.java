40d422d5f9933f7920d31014dd462e2e6f20b9b4
PioneerSwine996 <pioneerswine9@gmail.com>
2026-02-15 22:32:17 -0800

    Adjust GPV Camera Positioning

--- src/main/java/frc/robot/subsystems/gamepiecevision/GamePieceVisionConstants.java
+++ src/main/java/frc/robot/subsystems/gamepiecevision/GamePieceVisionConstants.java
@@ -58,10 +58,10 @@ public class GamePieceVisionConstants {
         GPVCam(
 
                 new Transform3d(
-                        Units.inchesToMeters(-6.625), Units.inchesToMeters(0.0), Units.inchesToMeters(27.25),
+                        Units.inchesToMeters(3.770), Units.inchesToMeters(-12.406), Units.inchesToMeters(20.485),
                         // Rotation order matters
-                        new Rotation3d(0.0, Units.degreesToRadians(45), 0.0)
-                                .rotateBy(new Rotation3d(0.0, 0.0, Units.degreesToRadians(180)))
+                        new Rotation3d(0.0, Units.degreesToRadians(6.963), 0.0)
+                                .rotateBy(new Rotation3d(0.0, 0.0, Units.degreesToRadians(10.0)))
                 ),
                 (cam) -> switch (BuildConstants.mode) {
                     case REAL -> new GamePieceVisionIOPhotonVision("GPVCam");
