4e57435a155d4138ca43b013edaffb0b6f0d6ad6
amsam0 <44983869+amsam0@users.noreply.github.com>
2026-02-27 10:28:53 -0800

    Remove Shootauto and rename Test to MarkAuto

--- src/main/java/frc/robot/autos/AutoManager.java
+++ src/main/java/frc/robot/autos/AutoManager.java
@@ -38,8 +38,7 @@ public class AutoManager {
         autoChooser.addOption("LeftSideAuto", LeftSideAuto.build());
         autoChooser.addOption("RightSideAuto", RightSideAuto.build());
         autoChooser.addOption("DepotAuto", DepotAuto.build());
-        autoChooser.addOption("Shootauto",Shootauto.build());
-        autoChooser.addOption("Test", Test.build());
+        autoChooser.addOption("MarkAuto", MarkAuto.build());
     }
 
     public Command getSelectedAuto() {
--- src/main/java/frc/robot/autos/MarkAuto.java
+++ src/main/java/frc/robot/autos/MarkAuto.java
@@ -17,7 +17,7 @@ import frc.robot.subsystems.superstructure.Superstructure;
 
 import java.util.function.Supplier;
 
-public class Shootauto {
+public class MarkAuto {
     private static final RobotState robotState = RobotState.get();
     private static final ShootingKinematics shootingKinematics = ShootingKinematics.get();
 
@@ -38,6 +38,7 @@ public class Shootauto {
                         Units.degreesToRadians(5)
                 ));
     }
+
     private static Command waypointintake(Translation2d translation2d) {
         Supplier<Pose2d> poseSupplier = () -> new Pose2d(
                 AllianceFlipUtil.apply(translation2d),
@@ -62,13 +63,17 @@ public class Shootauto {
                         superintake.setGoal(Superintake.Goal.INTAKE),
                         superstructure.setGoal(Superstructure.Goal.IDLE)
                 ),
-                Commands.parallel(
-                        waypoint(new Translation2d(3.8, 7.5))
-                                .andThen(waypoint(FieldConstants.Depot.depotCenter.toTranslation2d())),
+                Commands.race(
+                        waypoint(new Translation2d(3.8, 7.5)).andThen(
+                                waypoint(new Translation2d(1.275, 5.1))), //2.0 x 4.2 is good for y
                         superintake.setGoal(Superintake.Goal.INTAKE),
-                        superstructure.setGoal(Superstructure.Goal.SHOOT).withTimeout(3)
+                        superstructure.setGoal(Superstructure.Goal.SHOOT).withTimeout(3)),
+                Commands.race(
+                        waypoint(FieldConstants.Depot.depotCenter.toTranslation2d()).alongWith(
+                                superintake.setGoal(Superintake.Goal.INTAKE),
+                                superstructure.setGoal(Superstructure.Goal.SHOOT))
                 ),
-                waypoint(new Translation2d(3.25, 7.0))
+                waypoint(FieldConstants.Depot.depotCenter.toTranslation2d())
         );
     }
 }
