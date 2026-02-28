86a86454f73ef34ec1f1b630c2422114960fa30d
team955 <556881+team955@users.noreply.github.com>
2026-02-27 17:29:49 -0800

    Delete volatile test

--- src/test/java/frc/robot/subsystems/drive/DriveGoalNameTests.java
+++ src/test/java/frc/robot/subsystems/drive/DriveGoalNameTests.java
@@ -1,6 +1,5 @@
 package frc.robot.subsystems.drive;
 
-import frc.robot.subsystems.drive.goals.DriveJoystickGoal;
 import frc.robot.subsystems.drive.goals.FollowTrajectoryGoal;
 import org.junit.jupiter.api.Test;
 
@@ -9,7 +8,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class DriveGoalNameTests {
     @Test
     void driveGoalTests() {
-        assertEquals(new DriveJoystickGoal(false).loggableName, "DRIVE_JOYSTICK");
         assertEquals(new FollowTrajectoryGoal(null).loggableName, "FOLLOW_TRAJECTORY");
     }
 }
