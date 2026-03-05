57eaf6cf6f7556c12059a721f8306f30912c4920
team955 <556881+team955@users.noreply.github.com>
2026-03-04 18:45:57 -0800

    Reduce threshold for gyro discard

--- src/main/java/frc/robot/subsystems/drive/DriveConstants.java
+++ src/main/java/frc/robot/subsystems/drive/DriveConstants.java
@@ -22,7 +22,7 @@ public class DriveConstants {
     static final boolean useHighFrequencyOdometry = true;
 
     static final double odometryPositionDeltaDiscardMeters = Units.inchesToMeters(8);
-    static final double odometryGyroRotationDeltaDiscardRad = Units.degreesToRadians(45);
+    static final double odometryGyroRotationDeltaDiscardRad = Units.degreesToRadians(20);
 
     public static final DriveConfig driveConfig = new DriveConfig(
             // TO TUNE WHEEL RADIUS: Place robot on carpet and use wheel radius characterization auto.
