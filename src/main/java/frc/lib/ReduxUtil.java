6bab377bda1d68a0d42caff170464b3acfa4c4fc
deicide106 <choijustin1006@gmail.com>
2026-03-16 08:44:23 -0700

    redux util

---
+++ src/main/java/frc/lib/ReduxUtil.java
@@ -0,0 +1,23 @@
+package frc.lib;
+
+
+import java.util.function.Supplier;
+
+import static frc.lib.Util.asyncExecutor;
+
+public class ReduxUtil {
+
+    public static void tryUntilOk(int maxAttempts, Supplier<Boolean> command) {
+        for (int i = 0; i < maxAttempts; i++) {
+            var error = command.get();
+            if (error) return;
+        }
+        System.out.println("ReduxUtil tryUntilOk failure");
+
+
+    }
+
+    public static void tryUntilOkAsync(int maxAttempts, Supplier<Boolean> command) {
+        asyncExecutor.execute(() -> tryUntilOk(maxAttempts, command));
+    }
+}
