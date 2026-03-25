83f365af858f7509a1bb4fb6be77a958a1d916b3
PioneerSwine996 <pioneerswine9@gmail.com>
2026-03-25 10:56:05 -0700

    Update AuraAuto

--- src/main/java/frc/robot/autos/AuraAuto.java
+++ src/main/java/frc/robot/autos/AuraAuto.java
@@ -2,14 +2,17 @@ package frc.robot.autos;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import frc.lib.commands.CommandsExt;
 import frc.robot.FieldConstants;
 import frc.robot.subsystems.drive.Drive;
 import frc.robot.subsystems.superintake.Superintake;
 import frc.robot.subsystems.superstructure.Superstructure;
 
-import static frc.robot.subsystems.drive.DriveConstants.driveConfig;
+import static frc.robot.autos.AutoHelpers.intakeConstraints;
+import static frc.robot.subsystems.drive.DriveConstants.*;
 
 public class AuraAuto extends Auto {
     private static final Drive drive = Drive.get();
@@ -29,145 +32,110 @@ public class AuraAuto extends Auto {
 
     private static Command build() {
         return CommandsExt.eagerSequence(
-                /*
-                Commands.deadline(
-                        AutoHelpers.finalWaypoint(
-                                () -> new Pose2d(2.0, 5.0, Rotation2d.kZero),
-                                shootingConstraints
-                        ),
-                        superstructure.setGoal(Superstructure.Goal.SHOOT)
-                ),
-                superstructure.setGoal(Superstructure.Goal.IDLE).until(() -> true),
-
-                AutoHelpers.intermediateWaypoint(
-                        () -> new Pose2d(1.6, 5.0, Rotation2d.kCCW_90deg),
-                        intakeConstraints
-                ),
-
-                AutoHelpers.finalWaypoint(
-                        () -> new Pose2d(0.5, 4.8, Rotation2d.kCCW_90deg),
-                        intakeConstraints
-                ),
-
-                Commands.race(
-                        AutoHelpers.finalWaypoint(
-                                () -> new Pose2d(0.5, 6.5, Rotation2d.kCCW_90deg),
-                                intakeConstraints
-                        ),
-                        superintake.setGoal(Superintake.Goal.INTAKE)
-                ),
-                superintake.setGoal(Superintake.Goal.IDLE).until(() -> true),
-
-                // Move, intake, and shoot towards depot
-                //AutoHelpers.intakeFromDepotWhileShooting(shootingConstraints)
-                //        .withTimeout(5),
-                //
-                //// Move towards center
-                superstructure.setGoal(Superstructure.Goal.SHOOT).until(() -> true),
-                AutoHelpers.yDistanceInterpolatingWaypoint(
-                        new Translation2d(2.5, 5.5),
-                        new Translation2d(2.5, FieldConstants.LinesHorizontal.center),
-                        Rotation2d.kCW_90deg,
-                        1.0,
-                        shootingConstraints
-                ),
-
+                // Shoot while moving to outpost
                 Commands.race(
                         AutoHelpers.yDistanceInterpolatingWaypoint(
                                 new Translation2d(2.5, 1.0),
                                 new Translation2d(1.0, 1.0),
                                 new Rotation2d(),
                                 2.0,
-                                shootingConstraints
+                                shootingConstraints,
+                                true
                         ),
+                        superstructure.setGoal(Superstructure.Goal.SHOOT),
                         superintake.intakeShootAlternate()
                 ),
 
-                superintake.setGoal(Superintake.Goal.IDLE).until(() -> true),
-                superstructure.setGoal(Superstructure.Goal.IDLE).until(() -> true),
-                AutoHelpers.finalWaypoint(
-                        () -> new Pose2d(0.5, 0.7, Rotation2d.kCCW_90deg),
-                        defaultMoveToConstraints
+                // Line up to the Outpost
+                Commands.race(
+                        AutoHelpers.finalWaypoint(
+                                () -> new Pose2d(0.5, 0.7, Rotation2d.kCCW_90deg),
+                                defaultMoveToConstraints,
+                                false
+                        ),
+                        superintake.setGoal(Superintake.Goal.DEPLOY)
                 ),
+
+                // Wait at outpost
                 Commands.waitSeconds(2.5),
-                Commands.parallel(
+
+                // Move back to idle position
+                superintake.setGoal(Superintake.Goal.IDLE).until(() -> true),
+
+                // Move closer to the hub to move to depot
+                Commands.race(
                         AutoHelpers.finalWaypoint(
                                 () -> new Pose2d(1.5, 1.45, Rotation2d.kCCW_90deg),
-                                shootingConstraints
+                                shootingConstraints,
+                                true
                         ),
                         superintake.intakeShootAlternate(),
                         superstructure.setGoal(Superstructure.Goal.SHOOT)
+                ),
+
+                // Shoot while moving across to the depot
+                Commands.race(
+                        AutoHelpers.yDistanceInterpolatingWaypoint(
+                                new Translation2d(2.5, FieldConstants.LinesHorizontal.center),
+                                new Translation2d(2.5, 5.5),
+                                Rotation2d.kCW_90deg,
+                                1.0,
+                                shootingConstraints,
+                                true
+                        ),
+                        superstructure.setGoal(Superstructure.Goal.SHOOT)
+                ),
+
+                // Shoot while moving to depot
+                Commands.deadline(
+                        AutoHelpers.finalWaypoint(
+                                () -> new Pose2d(2.0, 5.0, Rotation2d.kZero),
+                                shootingConstraints,
+                                true
+                        ),
+                        superstructure.setGoal(Superstructure.Goal.SHOOT)
+                ),
+                superstructure.setGoal(Superstructure.Goal.IDLE).until(() -> true),
+
+                // Move toward depot
+                AutoHelpers.intermediateWaypoint(
+                        () -> new Pose2d(1.6, 5.0, Rotation2d.kCCW_90deg),
+                        intakeConstraints,
+                        false
+                ),
+
+                // Move to side of depot for easier intaking
+                AutoHelpers.finalWaypoint(
+                        () -> new Pose2d(0.5, 4.8, Rotation2d.kCCW_90deg),
+                        intakeConstraints,
+                        false
+                ),
+
+                // Intake the Depot
+                Commands.race(
+                        AutoHelpers.finalWaypoint(
+                                () -> new Pose2d(0.5, 6.5, Rotation2d.kCCW_90deg),
+                                intakeConstraints,
+                                false
+                        ),
+                        superintake.setGoal(Superintake.Goal.INTAKE)
+                ),
+
+                // Stop Intaking
+                superintake.setGoal(Superintake.Goal.IDLE).until(() -> true),
+
+                // Shoot while moving to center line
+                Commands.race(
+                        AutoHelpers.yDistanceInterpolatingWaypoint(
+                                new Translation2d(2.5, 5.5),
+                                new Translation2d(2.5, FieldConstants.LinesHorizontal.center),
+                                Rotation2d.kCW_90deg,
+                                1.0,
+                                shootingConstraints,
+                                true
+                        ),
+                        superstructure.setGoal(Superstructure.Goal.SHOOT)
                 )
-
-                //drive.moveTo(
-                //        () -> AutoHelpers.getIntakePose(
-                //                new Bounds(
-                //                        driveConfig.bumperLengthMeters() / 2.0,
-                //                        1.4,
-                //                        driveConfig.bumperWidthMeters() / 2.0,
-                //                        1.8
-                //                ),
-                //                AutoHelpers.yDistanceInterpolation(
-                //                        new Translation2d(2.5, 1.0),
-                //                        new Translation2d(1.0, 1.0),
-                //                        new Rotation2d(),
-                //                        2.0
-                //                )
-                //        ),
-                //        shootingConstraints
-                //)
-
-                //// Move forward to shooting position
-                //AutoHelpers.finalWaypoint(() -> shootingPosition, defaultMoveToConstraints),
-                //
-                //// Shoot for 1.5 sec
-                //Commands.parallel(
-                //        superintake.setGoal(Superintake.Goal.INTAKE),
-                //        superstructure.setGoal(Superstructure.Goal.SHOOT),
-                //        AutoHelpers.finalWaypoint(() -> shootingPosition, defaultMoveToConstraints.withAiming(true))
-                //).withTimeout(1.5),
-                //
-                //// Go to the outpost and wait at outpost for ~3 sec while intaking
-                //Commands.parallel(
-                //        superintake.setGoal(Superintake.Goal.INTAKE),
-                //        superstructure.setGoal(Superstructure.Goal.SHOOT),
-                //        AutoHelpers.finalWaypoint(() -> outpostPosition, defaultMoveToConstraints)
-                //).withTimeout(4),
-                //
-                //// Move back to shooting position
-                //Commands.parallel(
-                //        superintake.setGoal(Superintake.Goal.INTAKE),
-                //        superstructure.setGoal(Superstructure.Goal.SHOOT),
-                //        AutoHelpers.finalWaypoint(() -> shootingPosition, defaultMoveToConstraints.withAiming(true))
-                //).withTimeout(1.5),
-                //
-                //// Shoot for ~1.5 sec
-                //Commands.parallel(
-                //        superintake.setGoal(Superintake.Goal.INTAKE),
-                //        superstructure.setGoal(Superstructure.Goal.SHOOT),
-                //        AutoHelpers.finalWaypoint(() -> shootingPosition, defaultMoveToConstraints.withAiming(true))
-                //).withTimeout(1.5),
-                //
-                //// Go to the depot while intaking
-                //Commands.parallel(
-                //        superintake.setGoal(Superintake.Goal.INTAKE),
-                //        superstructure.setGoal(Superstructure.Goal.SHOOT),
-                //        Commands.sequence(
-                //                AutoHelpers.finalWaypoint(() -> depotPosition, AutoHelpers.intakeConstraints),
-                //                AutoHelpers.finalWaypoint(() -> depotIntakePosition, AutoHelpers.intakeConstraints)
-                //        )
-                //).withTimeout(3),
-                //
-                //// Go back to shooting position
-                //AutoHelpers.finalWaypoint(() -> shootingPosition, defaultMoveToConstraints.withAiming(true)),
-                //
-                //// Shoot
-                //Commands.parallel(
-                //        superintake.setGoal(Superintake.Goal.INTAKE),
-                //        superstructure.setGoal(Superstructure.Goal.SHOOT),
-                //        AutoHelpers.finalWaypoint(() -> shootingPosition, defaultMoveToConstraints.withAiming(true))
-                //).withTimeout(5)
-                 */
         );
     }
 }
