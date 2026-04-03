d129a411b04fd5193797e610511f78bab8795f9c
amsam0 <44983869+amsam0@users.noreply.github.com>
2026-04-02 21:39:14 -0700

    Add gamepiecevision project, including native-in-jar bundling

---
+++ gamepiecevision/src/main/java/frc955/gamepiecevision/Main.java
@@ -0,0 +1,18 @@
+// Copyright (c) FIRST and other WPILib contributors.
+
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc955.gamepiecevision;
+
+
+import org.photonvision.jni.LibraryLoader;
+
+public final class Main {
+    private Main() {
+    }
+
+    public static void main(String... args) {
+        LibraryLoader.loadWpiLibraries();
+    }
+}
