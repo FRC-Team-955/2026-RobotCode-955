75d5bef1e7fcd2505e6c219421f6fa94b5ce1b33
amsam0 <44983869+amsam0@users.noreply.github.com>
2026-04-03 21:27:18 -0700

    Get PhotonCamera working

--- gamepiecevision/src/main/java/frc955/gamepiecevision/Main.java
+++ gamepiecevision/src/main/java/frc955/gamepiecevision/Main.java
@@ -6,8 +6,13 @@
 package frc955.gamepiecevision;
 
 
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.RobotBase;
 import org.photonvision.jni.LibraryLoader;
+import org.photonvision.jni.TimeSyncServer;
+import org.photonvision.timesync.TimeSyncSingleton;
+
+import java.lang.reflect.Field;
 
 public final class Main {
     private Main() {
@@ -17,6 +22,25 @@ public final class Main {
         // Load natives
         LibraryLoader.loadWpiLibraries();
         LibraryLoader.loadTargeting();
+
+        // Setup NetworkTables
+        NetworkTableInstance inst = NetworkTableInstance.getDefault();
+        inst.startClient4("frc955.gamepiecevision");
+        inst.setServerTeam(955);
+        if (System.getenv("GPVLOCAL") != null) {
+            inst.setServer("127.0.0.1");
+        }
+
+        // Manually create TimeSyncSingleton instance to avoid attempt to load photontargetingJNI
+        try {
+            var instance = TimeSyncSingleton.class.getDeclaredField("INSTANCE");
+            instance.setAccessible(true);
+            var server = new TimeSyncServer(5810);
+            server.start();
+            instance.set(null, server);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
 
         // Start code
         RobotBase.startRobot(Robot::new);
--- gamepiecevision/src/main/java/frc955/gamepiecevision/Robot.java
+++ gamepiecevision/src/main/java/frc955/gamepiecevision/Robot.java
@@ -1,12 +1,16 @@
 package frc955.gamepiecevision;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import org.photonvision.PhotonCamera;
 
 public class Robot extends TimedRobot {
     public Robot() {
     }
 
+    PhotonCamera c = new PhotonCamera("IntakeCam");
+
     @Override
     public void robotPeriodic() {
+        System.out.println(c.isConnected());
     }
 }
