988095b93b4ecec0837c5c0c0a9264c3cd5ce095
amsam0 <44983869+amsam0@users.noreply.github.com>
2026-08-30 12:08:16 -0700

    Port feeder to motor API

--- src/main/java/frc/robot/subsystems/superstructure/Feeder.java
+++ src/main/java/frc/robot/subsystems/superstructure/Feeder.java
@@ -1,17 +1,16 @@
-package frc.robot.subsystems.superstructure.feeder;
+package frc.robot.subsystems.superstructure;
 
 import com.ctre.phoenix6.signals.NeutralModeValue;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.util.Units;
-import edu.wpi.first.wpilibj.Alert;
 import edu.wpi.first.wpilibj.DriverStation;
 import frc.lib.EnergyLogger;
 import frc.lib.Util;
-import frc.lib.motor.MotorIO;
-import frc.lib.motor.MotorIOInputsAutoLogged;
-import frc.lib.motor.RequestType;
+import frc.lib.devices.motor.CtrlSparkMaxConfig;
+import frc.lib.devices.motor.MechanismSim;
+import frc.lib.devices.motor.Motor;
 import frc.lib.network.LoggedTunableNumber;
 import frc.lib.subsystem.Periodic;
 import frc.robot.BuildConstants;
@@ -23,8 +22,6 @@ import org.littletonrobotics.junction.Logger;
 
 import java.util.function.DoubleSupplier;
 
-import static frc.robot.subsystems.superstructure.feeder.FeederConstants.createIO;
-
 public class Feeder implements Periodic {
     private static final LoggedTunableNumber feedVoltage = new LoggedTunableNumber("Superstructure/Feeder/Goal/FeedVoltage", 12.0);
     private static final LoggedTunableNumber ejectVoltage = new LoggedTunableNumber("Superstructure/Feeder/Goal/EjectVoltage", -12.0);
@@ -32,26 +29,33 @@ public class Feeder implements Periodic {
     private static final EnergyLogger energyLogger = EnergyLogger.get();
     private static final OperatorDashboard operatorDashboard = OperatorDashboard.get();
 
-    private final MotorIO io = createIO();
-    private final MotorIOInputsAutoLogged inputs = new MotorIOInputsAutoLogged();
+    private final Motor motor = Motor
+            .createSparkMax(
+                    "Superstructure/Feeder",
+                    11,
+                    new CtrlSparkMaxConfig()
+                            .withInverted(true)
+                            .withNeutralMode(NeutralModeValue.Brake)
+                            .withCurrentLimit(40)
+                            .withGearRatio(3),
+                    0.0,
+                    MechanismSim.roller(0.01)
+            );
 
     @RequiredArgsConstructor
     public enum Goal {
-        IDLE(() -> 0, RequestType.VoltageVolts),
-        FEED(feedVoltage::get, RequestType.VoltageVolts),
-        EJECT(ejectVoltage::get, RequestType.VoltageVolts),
+        IDLE(() -> 0),
+        FEED(feedVoltage::get),
+        EJECT(ejectVoltage::get),
         ;
 
         /** Should be constant for every loop cycle */
-        private final DoubleSupplier value;
-        private final RequestType type;
+        private final DoubleSupplier voltageVolts;
     }
 
     @Setter
     @Getter
     private Goal goal = Goal.IDLE;
-
-    private final Alert motorDisconnectedAlert = new Alert("Feeder motor is disconnected.", Alert.AlertType.kError);
 
     private static Feeder instance;
 
@@ -71,16 +75,11 @@ public class Feeder implements Periodic {
 
     @Override
     public void periodicBeforeCommands() {
-        io.updateInputs(inputs);
-        Logger.processInputs("Inputs/Superstructure/Feeder", inputs);
-
-        motorDisconnectedAlert.set(!inputs.connected);
-
-        energyLogger.reportPowerUsage("Feeder", inputs.connected ? inputs.appliedVolts * inputs.supplyCurrentAmps : 0.0);
+        energyLogger.reportPowerUsage("Feeder", motor.isConnected() ? motor.getAppliedVolts() * motor.getSupplyCurrentAmps() : 0.0);
 
         // Apply network inputs
         if (operatorDashboard.coastOverride.hasChanged()) {
-            io.setNeutralMode(operatorDashboard.coastOverride.get() ? NeutralModeValue.Coast : NeutralModeValue.Brake);
+            motor.setNeutralMode(operatorDashboard.coastOverride.get() ? NeutralModeValue.Coast : NeutralModeValue.Brake);
         }
     }
 
@@ -88,23 +87,18 @@ public class Feeder implements Periodic {
     public void periodicAfterCommands() {
         Logger.recordOutput("Superstructure/Feeder/Goal", goal);
         if (DriverStation.isDisabled()) {
-            io.setRequest(RequestType.VoltageVolts, 0);
+            motor.setVoltageRequest(0);
         } else {
-            double value = goal.value.getAsDouble();
-            io.setRequest(goal.type, value);
+            double volts = goal.voltageVolts.getAsDouble();
+            motor.setVoltageRequest(volts);
             if (BuildConstants.isSimOrReplay) {
-                Logger.recordOutput("Superstructure/Feeder/RequestType", goal.type);
-                Logger.recordOutput("Superstructure/Feeder/RequestValue", value);
+                Logger.recordOutput("Superstructure/Feeder/RequestedVolts", volts);
             }
         }
     }
 
-    public double getPositionRad() {
-        return inputs.positionRad;
-    }
-
     public boolean isDisconnected() {
-        return !inputs.connected;
+        return !motor.isConnected();
     }
 
     public Transform3d transform() {
@@ -113,7 +107,7 @@ public class Feeder implements Periodic {
                 new Rotation3d(0.0, 0.0, Units.degreesToRadians(90.0))
         ).plus((new Transform3d(
                 new Translation3d(),
-                new Rotation3d(0.0, getPositionRad(), 0.0)
+                new Rotation3d(0.0, motor.getPositionRad(), 0.0)
         )));
     }
 }
