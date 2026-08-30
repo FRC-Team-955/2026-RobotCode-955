615c3facd4825b275e52b0faf926c994bcc9c158
amsam0 <44983869+amsam0@users.noreply.github.com>
2026-08-30 11:08:28 -0700

    Port spindexer to motor API

--- src/main/java/frc/robot/subsystems/superstructure/Spindexer.java
+++ src/main/java/frc/robot/subsystems/superstructure/Spindexer.java
@@ -1,21 +1,24 @@
-package frc.robot.subsystems.superstructure.spindexer;
+package frc.robot.subsystems.superstructure;
 
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.FeedbackConfigs;
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.util.Units;
-import edu.wpi.first.wpilibj.Alert;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Timer;
 import frc.lib.EnergyLogger;
 import frc.lib.Util;
-import frc.lib.motor.MotorIO;
-import frc.lib.motor.MotorIOInputsAutoLogged;
-import frc.lib.motor.RequestType;
+import frc.lib.devices.motor.MechanismSim;
+import frc.lib.devices.motor.Motor;
 import frc.lib.network.LoggedTunableNumber;
 import frc.lib.subsystem.Periodic;
 import frc.robot.BuildConstants;
-import frc.robot.OperatorDashboard;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -23,37 +26,44 @@ import org.littletonrobotics.junction.Logger;
 
 import java.util.function.DoubleSupplier;
 
-import static frc.robot.subsystems.superstructure.spindexer.SpindexerConstants.createIO;
-
 public class Spindexer implements Periodic {
     private static final LoggedTunableNumber feedVoltage = new LoggedTunableNumber("Superstructure/Spindexer/Goal/FeedVoltage", 12.0);
     private static final LoggedTunableNumber ejectVoltage = new LoggedTunableNumber("Superstructure/Spindexer/Goal/EjectVoltage", -12.0);
 
-    private static final OperatorDashboard operatorDashboard = OperatorDashboard.get();
     private static final EnergyLogger energyLogger = EnergyLogger.get();
 
-    private final MotorIO io = createIO();
-    private final MotorIOInputsAutoLogged inputs = new MotorIOInputsAutoLogged();
+    private final Motor motor = Motor.createTalonFX(
+            "Superstructure/Spindexer",
+            13,
+            new TalonFXConfiguration()
+                    .withMotorOutput(new MotorOutputConfigs()
+                            .withNeutralMode(NeutralModeValue.Coast)
+                            .withInverted(InvertedValue.Clockwise_Positive))
+                    .withCurrentLimits(new CurrentLimitsConfigs()
+                            .withStatorCurrentLimit(90)
+                            .withSupplyCurrentLimit(50))
+                    .withFeedback(new FeedbackConfigs()
+                            .withSensorToMechanismRatio(5)),
+            0.0,
+            MechanismSim.roller(0.01)
+    );
 
     @RequiredArgsConstructor
     public enum Goal {
-        IDLE(() -> 0, RequestType.VoltageVolts),
-        FEED(() -> /*Timer.getTimestamp() % 2.0 < 0.1 ? -feedVoltage.get() :*/ feedVoltage.get(), RequestType.VoltageVolts),
-        EJECT(ejectVoltage::get, RequestType.VoltageVolts),
-        EJECT_ALTERNATE(() -> Timer.getTimestamp() % 0.5 < 0.25 ? ejectVoltage.get() : -ejectVoltage.get(), RequestType.VoltageVolts),
-        AGITATE(() -> Timer.getTimestamp() % 3.0 < 1.0 ? Math.sin(2.0 * Math.PI * Timer.getTimestamp()) : 0.0, RequestType.VoltageVolts),
+        IDLE(() -> 0),
+        FEED(() -> /*Timer.getTimestamp() % 2.0 < 0.1 ? -feedVoltage.get() :*/ feedVoltage.get()),
+        EJECT(ejectVoltage::get),
+        EJECT_ALTERNATE(() -> Timer.getTimestamp() % 0.5 < 0.25 ? ejectVoltage.get() : -ejectVoltage.get()),
+        AGITATE(() -> Timer.getTimestamp() % 3.0 < 1.0 ? Math.sin(2.0 * Math.PI * Timer.getTimestamp()) : 0.0),
         ;
 
         /** Should be constant for every loop cycle */
-        private final DoubleSupplier value;
-        private final RequestType type;
+        private final DoubleSupplier voltageVolts;
     }
 
     @Setter
     @Getter
     private Goal goal = Goal.IDLE;
-
-    private final Alert motorDisconnectedAlert = new Alert("Spindexer motor is disconnected.", Alert.AlertType.kError);
 
     private static Spindexer instance;
 
@@ -73,35 +83,25 @@ public class Spindexer implements Periodic {
 
     @Override
     public void periodicBeforeCommands() {
-        io.updateInputs(inputs);
-        Logger.processInputs("Inputs/Superstructure/Spindexer", inputs);
-
-        motorDisconnectedAlert.set(!inputs.connected);
-
-        energyLogger.reportPowerUsage("Spindexer", inputs.connected ? inputs.appliedVolts * inputs.supplyCurrentAmps : 0.0);
+        energyLogger.reportPowerUsage("Spindexer", motor.isConnected() ? motor.getAppliedVolts() * motor.getSupplyCurrentAmps() : 0.0);
     }
 
     @Override
     public void periodicAfterCommands() {
         Logger.recordOutput("Superstructure/Spindexer/Goal", goal);
         if (DriverStation.isDisabled()) {
-            io.setRequest(RequestType.VoltageVolts, 0);
+            motor.setVoltageRequest(0);
         } else {
-            double value = goal.value.getAsDouble();
-            io.setRequest(goal.type, value);
+            double voltageVolts = goal.voltageVolts.getAsDouble();
+            motor.setVoltageRequest(voltageVolts);
             if (BuildConstants.isSimOrReplay) {
-                Logger.recordOutput("Superstructure/Spindexer/RequestType", goal.type);
-                Logger.recordOutput("Superstructure/Spindexer/RequestValue", value);
+                Logger.recordOutput("Superstructure/Spindexer/RequestedVolts", voltageVolts);
             }
         }
     }
 
-    public double getPositionRad() {
-        return inputs.positionRad;
-    }
-
     public boolean isDisconnected() {
-        return !inputs.connected;
+        return !motor.isConnected();
     }
 
     public Transform3d transform() {
@@ -110,7 +110,7 @@ public class Spindexer implements Periodic {
                 new Rotation3d(0.0, 0.0, Units.degreesToRadians(90.0))
         ).plus(new Transform3d(
                 new Translation3d(),
-                new Rotation3d(0.0, getPositionRad(), 0.0)
+                new Rotation3d(0.0, motor.getPositionRad(), 0.0)
         ));
     }
 }
